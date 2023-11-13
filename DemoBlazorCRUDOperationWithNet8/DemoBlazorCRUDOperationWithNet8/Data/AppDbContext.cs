d1891f12a7fe690ed6d10b8e4ea071f91ad08e5b
Emilio Barrera Sepúlveda <barrera_emilio@yahoo.es>
2023-11-13 22:28:12 +0000

    add AppDbContext

---
+++ DemoBlazorCRUDOperationWithNet8/DemoBlazorCRUDOperationWithNet8/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using DemoBlazorCRUDOperationWithNet8.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoBlazorCRUDOperationWithNet8.Data
+{
+    public class AppDbContext:DbContext 
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
