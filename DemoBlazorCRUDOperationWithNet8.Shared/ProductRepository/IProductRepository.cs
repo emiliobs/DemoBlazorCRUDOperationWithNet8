a55eb12a4f38f83ecccbcc5db1196043136092e1
Emilio Barrera Sepúlveda <barrera_emilio@yahoo.es>
2023-11-14 01:38:27 +0000

    IProductRepository and ProductRepository done

---
+++ DemoBlazorCRUDOperationWithNet8.Shared/ProductRepository/IProductRepository.cs
@@ -0,0 +1,27 @@
+﻿using DemoBlazorCRUDOperationWithNet8.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoBlazorCRUDOperationWithNet8.Shared.ProductRepository
+{
+    public interface IProductRepository
+    {
+       Task<Product> AddProductAsync(Product product);  
+
+       Task<Product> UpdateProductAsync(Product product);
+        
+       Task DeleteProductAsync(int productId); 
+       
+       Task<Product> GetAllProductAsync();
+        
+       Task<Product> GetProductByIdAsync(int productId);
+
+
+
+
+
+    }
+}
---
+++ DemoBlazorCRUDOperationWithNet8/DemoBlazorCRUDOperationWithNet8/Implementations/ProductRepository.cs
@@ -0,0 +1,41 @@
+﻿using DemoBlazorCRUDOperationWithNet8.Data;
+using DemoBlazorCRUDOperationWithNet8.Shared.Models;
+using DemoBlazorCRUDOperationWithNet8.Shared.ProductRepository;
+
+namespace DemoBlazorCRUDOperationWithNet8.Implementations
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ProductRepository(AppDbContext context)
+        {
+            this._context = context;
+        }
+
+        public Task<Product> AddProductAsync(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteProductAsync(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetAllProductAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetProductByIdAsync(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> UpdateProductAsync(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
